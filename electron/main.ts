857b489751cc38d9816a24ec6e626f671079d4d5
fan <291950304@qq.com>
2024-10-25 17:30:45 +0800

    chore: main render

--- electron/main.ts
+++ electron/main.ts
@@ -1,9 +1,12 @@
-import { app, BrowserWindow } from 'electron'
-// import { createRequire } from 'node:module'
+import { app, BrowserWindow, ipcMain, dialog } from 'electron'
+import { createRequire } from 'node:module'
 import { fileURLToPath } from 'node:url'
 import path from 'node:path'
+import fs from 'node:fs'
+import OpenAI from 'openai'
+import { useOpenAI } from '../src/hooks/openai'
 
-// const require = createRequire(import.meta.url)
+const require = createRequire(import.meta.url)
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 // The built directory structure
@@ -67,4 +70,104 @@ app.on('activate', () => {
   }
 })
 
-app.whenReady().then(createWindow)
+// 选择文件
+ipcMain.handle('select-file', async () => {
+  const result = await dialog.showOpenDialog({
+    /**
+     * openFile 打开文件
+     * multiSelections 多选
+     */
+    properties: ['openFile', 'multiSelections']
+  })
+  return result.filePaths
+})
+
+// 选择文件夹
+ipcMain.handle('select-folder', async () => {
+  const result = await dialog.showOpenDialog({
+    properties: ['openDirectory']
+  })
+  return result.filePaths
+})
+
+// 获取所有文件
+function getAllFiles(dir: string): string[] {
+  const files = fs.readdirSync(dir)
+  let allFiles: string[] = []
+  
+  files.forEach(file => {
+    const filePath = path.join(dir, file)
+    if (fs.statSync(filePath).isDirectory()) {
+      allFiles = allFiles.concat(getAllFiles(filePath))
+    } else {
+      allFiles.push(filePath)
+    }
+  })
+  
+  return allFiles
+}
+
+// 获取文件
+ipcMain.handle('get-files', async (_, paths: string[]) => {
+  let allFiles: string[] = []
+  
+  for (const path of paths) {
+    if (fs.statSync(path).isDirectory()) {
+      allFiles = allFiles.concat(getAllFiles(path))
+    } else {
+      allFiles.push(path)
+    }
+  }
+  
+  return allFiles
+})
+
+// 转换文件
+ipcMain.handle('convert-file', async (_, inputPath, outputPath) => {
+  console.log(inputPath, outputPath);
+  // 判断inputPath是否为视频
+  const isVideo = inputPath.endsWith('.mp4') || inputPath.endsWith('.avi') || inputPath.endsWith('.mov')
+  const isAudio = inputPath.endsWith('.mp3') || inputPath.endsWith('.wav') || inputPath.endsWith('.m4a')
+  console.log(isVideo, isAudio);
+  if (isVideo) {
+    // 将 视频 转换为 音频
+    const FfmpegCommand  = require('fluent-ffmpeg')
+    const ffmpeg = new FfmpegCommand()
+    return new Promise((resolve, reject) => {
+      ffmpeg.setFfmpegPath(require('ffmpeg-static'))
+        .addInput(inputPath)
+        .output(outputPath)
+        .on('end', () => resolve('转换完成'))
+        .on('error', (err: Error) => reject(err))
+        .run()
+    })
+  } else if (isAudio) {
+    // 将 音频 转换为 文字
+    // const apiKey = 'sk-7DYgNAYx145IBacrEeCc630bF63c4138AbFf7b9bBa76F025'
+    const { getApiKey, getBaseURL } = useOpenAI()
+    const apiKey = await getApiKey()
+    const baseURL = await getBaseURL()
+    const openai = new OpenAI({
+      apiKey,
+      baseURL,
+      // baseURL: 'https://free.gpt.ge/v1'
+    })
+    return new Promise((resolve, reject) => {
+      openai.audio.transcriptions.create({
+        file: fs.createReadStream(inputPath),
+        model: "whisper-1",
+        response_format: outputPath.includes('.srt') ? 'srt' : 'text',
+        language: 'zh'
+      }).then(res => {
+        console.log(res);
+        fs.writeFileSync(outputPath, res)
+        resolve('转换完成')
+      }).catch(err => {
+        console.log(err);
+        reject(err)
+      })
+    })
+  }
+})
+
+app.whenReady().then(() => createWindow())
