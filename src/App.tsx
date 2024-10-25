4fb95d92d077bea822bfe577b39a5b153abac229
fan <291950304@qq.com>
2024-10-25 17:30:31 +0800

    chore: 渲染组件

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import Tools from '@/components/tools.tsx'
+import Rules from '@/components/rules.tsx'
+import FileList from '@/components/file-list.tsx'
 
 function App() {
   return (
     <>
-      <Tools/>
+      <Tools />
+      <Rules />
+      <FileList />
     </>
   )
 }
