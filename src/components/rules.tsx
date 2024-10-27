ce90f99e24d1a8d9103f14d1b7656ab4b0aeb166
fan <291950304@qq.com>
2024-10-27 20:47:25 +0800

    chore: 转换规则

--- src/components/rules.tsx
+++ src/components/rules.tsx
@@ -1,57 +1,75 @@
-import { useState, useEffect } from "react"
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select"
+import { useState } from "react"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
-import {OutputType, useFileStore } from "@/hooks/file-store"
-import { useOpenAI } from "@/hooks/openai"
+import { AUDIO_OUTPUT_TYPES, TEXT_OUTPUT_TYPES, AudioOutputType, TextOutputType, OutputType, useFileStore } from "@/hooks/file-store"
 import { Button } from "@/components/ui/button"
+import { OpenAIEnum } from "@/types/openai"
+import { ButtonGroup, ButtonGroupItem } from "@/components/radio-button-group"
+
 function Rules() {
-  // const { outputType, setOutputType } = useFileStore()
-  const { getApiKey, getBaseURL, setApiKey, setBaseURL } = useOpenAI()
+  const { setOutputType } = useFileStore()
   const [activeTab, setActiveTab] = useState<string>('audio')
-  const [openAIKey, setOpenAIKey] = useState<string>(async () => await getApiKey() || '')
-  const [openAIBaseURL, setOpenAIBaseURL] = useState<string>(async () => await getBaseURL() || '')
-  useEffect(() => {
-    getApiKey().then((key) => setOpenAIKey(key || ''))
-    getBaseURL().then((url) => setOpenAIBaseURL(url || ''))
-  }, [])
+  const [openAIKey, setOpenAIKey] = useState<string>(() => localStorage.getItem(OpenAIEnum.API_KEY) || '')
+  const [openAIBaseURL, setOpenAIBaseURL] = useState<string>(() => localStorage.getItem(OpenAIEnum.BASE_URL) || '')
+
+  const [audioOutputType, setAudioOutputType] = useState<AudioOutputType>(AUDIO_OUTPUT_TYPES[0])
+  const [textOutputType, setTextOutputType] = useState<TextOutputType>(TEXT_OUTPUT_TYPES[0])
   const save = () => {
     if (openAIKey && openAIBaseURL) {
-      setApiKey(openAIKey)
-      setBaseURL(openAIBaseURL)
+      localStorage.setItem(OpenAIEnum.API_KEY, openAIKey)
+      localStorage.setItem(OpenAIEnum.BASE_URL, openAIBaseURL)
     }
   }
   return (
     <div className="space-y-2 mx-5 py-5">
       <div>
-        <Tabs value={activeTab} onValueChange={setActiveTab}>
+        <Tabs value={activeTab} onValueChange={(value) => {
+          setActiveTab(value)
+          setOutputType(value === 'audio' ? audioOutputType : textOutputType)
+        }}>
           <TabsList className="grid w-full grid-cols-2">
             <TabsTrigger value="audio">转音频</TabsTrigger>
             <TabsTrigger value="text">转文本</TabsTrigger>
           </TabsList>
           <TabsContent value="audio">
-            <div>
+            <div className="flex items-center">
               <Label htmlFor="output-type" className="flex-shrink-0 mr-2 font-bold">转换类型:</Label>
-              {
-                // 实现单选按钮组
-                Object.values(OutputType).map((type) => (
-                  <div key={type}>
-                    {type}
-                  </div>
-                ))
-              }
+              <ButtonGroup value={audioOutputType} onValueChange={(value) => {
+                setAudioOutputType(value as AudioOutputType)
+                setOutputType(value as OutputType)
+              }}>
+                {AUDIO_OUTPUT_TYPES.map((type) => (
+                  <ButtonGroupItem key={type} value={type} icon={<></>} label={type} />
+                ))}
+              </ButtonGroup>
             </div>
           </TabsContent>
 
           <TabsContent value="text">
             <div className="flex items-center">
+              <Label htmlFor="output-type" className="flex-shrink-0 mr-2 font-bold">转换类型:</Label>
+              <ButtonGroup value={textOutputType} onValueChange={(value) => {
+                setTextOutputType(value as TextOutputType)
+                setOutputType(value as OutputType)
+              }}>
+                {TEXT_OUTPUT_TYPES.map((type) => (
+                  <ButtonGroupItem key={type} value={type} icon={<></>} label={type} />
+                ))}
+              </ButtonGroup>
+            </div>
+            <div className="flex items-center my-2">
               <Label htmlFor="openai-base-url" className="flex-shrink-0 mr-2">baseURL</Label>
-              <Input id="openai-base-url" type="text" value={openAIKey} onChange={(e) => setOpenAIBaseURL(e.target.value)} placeholder="输入您的 OpenAI baseURL" />
+              <Input 
+              id="openai-base-url" 
+              type="text" 
+              value={openAIKey} onChange={(e) => setOpenAIKey(e.target.value)} 
+              placeholder="输入您的 OpenAI baseURL"
+              />
             </div>
             <div className="flex items-center">
               <Label htmlFor="openai-key" className="flex-shrink-0 mr-2">OpenAI 密钥</Label>
-              <Input id="openai-key" type="password" value={openAIBaseURL} onChange={(e) => setOpenAIKey(e.target.value)} placeholder="输入您的 OpenAI 密钥" />
+              <Input id="openai-key" type="password" value={openAIBaseURL} onChange={(e) => setOpenAIBaseURL(e.target.value)} placeholder="输入您的 OpenAI 密钥" />
             </div>
             <Button onClick={save}>保存</Button>
           </TabsContent>
